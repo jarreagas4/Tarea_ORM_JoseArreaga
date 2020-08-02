ca8814edce3713292cc80678cf8bed648ad322c5
jarreagas4 <jarreagas4@unemi.edu.ec>
2020-08-01 20:36:14 -0500

    Tarea_ORM_ArreagaJose

---
+++ TareaORM/app/admin.py
@@ -0,0 +1,8 @@
+from .models import Producto,Cliente,Factura,DetalleFactura
+from django.contrib import admin
+
+admin.site.register(Producto)
+admin.site.register(Cliente)
+admin.site.register(Factura)
+admin.site.register(DetalleFactura)
+
